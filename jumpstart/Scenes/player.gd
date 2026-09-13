7d72bccf08edb11721694bd11adc3bdb7dc2172c
Cassius <cashjackc@gmail.com>
2026-09-13 17:34:48 -0400

    add sprinting

--- jumpstart/Scenes/player.gd
+++ jumpstart/Scenes/player.gd
@@ -2,10 +2,17 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
+const sprintSpeed = 700
+var curSpeed = SPEED
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("sprint"):
+		curSpeed = sprintSpeed
+	else:
+		curSpeed = SPEED
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -18,8 +25,8 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * curSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, curSpeed)
 
 	move_and_slide()
